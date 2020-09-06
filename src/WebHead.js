be5c9526cb04ff4aa8363ae1def46606777bbc8b
Hammad Ashraf <hammadashraf971@gmail.com>
2020-09-06 19:08:49 +0500

    ten

--- src/WebHead.js
+++ src/WebHead.js
@@ -8,15 +8,12 @@ function WebHead() {
         <div>
 
             <Navbar  sticky="top"  className="fixed-top"  collapseOnSelect expand="lg" style={{backgroundColor:'#0099FF',padding:'10px',fontSize:'25px',justifyContent:'center'}} variant="light">
- 
-<span style={{fontSize:'70px',textShadow:' 0 0 10px gold',lineHeight:'30px',fontWeight:'400',color:'gold'}}>N</span>
+<span style={{fontSize:'50px',textShadow:' 0 0 10px gold',lineHeight:'30px',fontWeight:'40',color:'gold'}}>N</span>
   <span  style={{fontSize:'25px',lineHeight:'10px',textShadow:' 0 0 10px white',fontFamily:'Ubuntu, "Trebuchet MS", Trebuchet, Verdana, Helvetica, Arial, sans-serif',fontWeight:'200',color:'white'}} >AWAB</span>
-
-
-  <span style={{paddingLeft:'8px',fontSize:'70px',lineHeight:'30px', textShadow:' 0 0 10px gold',fontWeight:'400',color:'gold'}} >P</span>
+  <span style={{paddingLeft:'8px',fontSize:'50px',lineHeight:'30px', textShadow:' 0 0 10px gold',fontWeight:'40',color:'gold'}} >P</span>
   <span style={{fontSize:'25px',lineHeight:'10px',textShadow:' 0 0 10px white',fontFamily:'Ubuntu, "Trebuchet MS", Trebuchet, Verdana, Helvetica, Arial, sans-serif',fontWeight:'200',color:'white'}} >HOTO</span>
 
-  <span style={{paddingLeft:'8px',fontSize:'70px',lineHeight:'30px', textShadow:' 0 0 10px gold',fontWeight:'400',color:'gold'}}>S</span>
+  <span style={{paddingLeft:'8px',fontSize:'50px',lineHeight:'30px', textShadow:' 0 0 10px gold',fontWeight:'40',color:'gold'}}>S</span>
   <span style={{fontSize:'25px',lineHeight:'10px',textShadow:' 0 0 10px white',fontFamily:'Ubuntu, "Trebuchet MS", Trebuchet, Verdana, Helvetica, Arial, sans-serif',fontWeight:'200',color:'white'}} >TUDIO™</span>
 
   
